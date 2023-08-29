32f2f8a4ef21ffae1e95b49bc665ccd9238d3848
brayan <108742281+brayanvasc@users.noreply.github.com>
2023-08-28 21:05:20 -0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,13 @@
+a = 33
+b = "33"
+
+if b > a:
+    print("b é maior que a")
+
+if b == a:
+    print("a e b são iguais")
+
+def my_function(fname):
+    my_function("Emilly")
+    my_function("Tobias")
+    my_function("Lucas")
